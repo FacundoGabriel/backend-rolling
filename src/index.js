cd2423b62a6c2d3a7279d48d99c09fa45390bf1e
Facundo <facundodelacruz01@gmail.com>
2025-05-04 06:22:11 -0300

    se agrega la rutas de registro

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const port = 4000
 
 app.use(express.json())
 
+app.use('/api/usuarios', require('./routes/usuarios.routes'))
+
 app.listen(port, ()=> {
     console.log('servidor prendido en el puerto: ', port)
 })
